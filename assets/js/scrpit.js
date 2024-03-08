3de9a30c105f26863e0853f6db373a82b3490a83
Adrian Espinosa <adrian_espinosa@mail.com>
2024-03-08 20:20:22 -0300

    Final Version

--- assets/js/scrpit.js
+++ assets/js/scrpit.js
@@ -9,3 +9,11 @@ $(document).ready(function(){
         alert("El correo fue enviado correctamente");
     })
 });
+/* Smootp Scrol */
+$('.carousel').on('slide.bs.carousel', function (e) {
+    var $target = $(e.relatedTarget);
+    var index = $target.index();
+    $('html, body').animate({
+      scrollTop: $target.offset().top
+    }, 800); // Duración del desplazamiento en milisegundos
+  });
